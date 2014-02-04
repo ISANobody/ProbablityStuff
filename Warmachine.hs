7ad89c4b9c64088274f1839582945d34b5cd44fd
Dennis Griffith <isanobody@gmail.com>
2014-02-04 13:43:10 -0600

    add headbutt

--- Warmachine.hs
+++ Warmachine.hs
@@ -33,51 +33,106 @@ module Warmachine where
   boostedDmg n = do r <- _3d6
                     return $ max (r+n) 0
 
-  basicAtk :: (Fractional prob, Ord prob) => Int -> Int -> T prob Int
-  basicAtk d a = do h <- attack d
-                    if h then damage a else 0
-
-  data Action = None | BoostAtk | BoostDmg | BoostBoth deriving (Ord, Eq, Show, Enum)
+  data Action = None | BoostAtk | BoostDmg | BoostBoth
+              | HeadButt | HeadBAtk | HeadBDmg | HeadBoth
+               deriving (Ord, Eq, Show, Enum)
 
   -- Should be in Prelude :(
   maximumWRT :: (Ord b) => (a -> b) -> [a] -> a
   maximumWRT f xs = maximumBy (\x y -> compare (f x) (f y)) xs
 
   -- This is ugly. I don't know why memoization didn't work on the cleaner version
-  memoed :: (Fractional p, Ord p) => Int -> Int -> Int -> (Action,p)
-  memoed d a = 
-    let f = memoize      $ \r -> case r of
-                                 0 -> (None,myExpected $ basicAtk d a)
-                                 1 -> maximumWRT snd [(None,(myExpected $ basicAtk d a) + (snd $ f 0))
-                                                     ,(BoostAtk,expected (batk 1 f))
-                                                     ,(BoostDmg,expected (bdmg 1 f))]
-                                 _ -> maximumWRT snd [(None,(myExpected $ basicAtk d a) + (snd $ f (r-1)))
-                                                     ,(BoostAtk,expected (batk r f))
-                                                     ,(BoostDmg,expected (bdmg r f))
-                                                     ,(BoostBoth,expected (both r f))]
+  normalAtk :: (Fractional p, Ord p) => Int -> Int -> Int -> Bool -> Int -> (Action,p)
+  normalAtk s d a = 
+    let f = memoize2 $ \ah r -> case r of
+                                 0 -> (None,expected (natk ah 0 f))
+                                 1 -> maximumWRT snd [(None,(expected (natk ah 1 f)))
+                                                     ,(BoostAtk,expected (batk ah 1 f))
+                                                     ,(BoostDmg,expected (bdmg ah 1 f))
+                                                     ,(HeadButt,expected (hdbt ah 1 f))]
+                                 2 -> maximumWRT snd [(None,(expected (natk ah r f)))
+                                                     ,(BoostAtk,expected (batk ah r f))
+                                                     ,(BoostDmg,expected (bdmg ah r f))
+                                                     ,(BoostBoth,expected (both ah r f))
+                                                     ,(HeadButt,expected (hdbt ah r f))
+                                                     ,(HeadBAtk,expected (hdba ah r f))
+                                                     ,(HeadBDmg,expected (hdbd ah r f))]
+                                 _ -> maximumWRT snd [(None,(expected (natk ah r f)))
+                                                     ,(BoostAtk,expected (batk ah r f))
+                                                     ,(BoostDmg,expected (bdmg ah r f))
+                                                     ,(BoostBoth,expected (both ah r f))
+                                                     ,(HeadButt,expected (hdbt ah r f))
+                                                     ,(HeadBAtk,expected (hdba ah r f))
+                                                     ,(HeadBDmg,expected (hdbd ah r f))
+                                                     ,(HeadBoth,expected (hdda ah r f))]
     in f
-   where batk 0 _ = error "Can't boost attack with no resource"
-         batk 1 _ = do h <- boostedAtk d
-                       return $ if h then (myExpected $ damage a) else 0
-         batk r f = do h <- boostedAtk d
-                       return $ (snd $ f (r-2)) + if h 
+   where natk ah 0 _ = do h <- attack d
+                          return $ if h || ah then (myExpected $ damage a) else 0
+         natk ah r f = do h <- attack d
+                          return $ if h || ah then (myExpected $ damage a) + (snd $ f ah (r-1)) 
+                                              else (snd $ f ah (r-1))
+         batk _ 0 _ = error "Can't boost attack with no resource"
+         batk ah 1 _ = do h <- boostedAtk d
+                          return $ if h || ah then (myExpected $ damage a) else 0
+         batk ah r f = do h <- boostedAtk d
+                          return $ (snd $ f ah (r-2)) + if h || ah
                                             then (myExpected $ damage a) 
                                             else 0
-         bdmg 0 _ = error "Can't boost damage with no resource"
-         bdmg 1 f = do h <- attack d
-                       return $ if h then (myExpected $ boostedDmg a) 
-                                     else snd $ f 0
-         bdmg r f = do h <- attack d
-                       return $ if h 
-                                then (myExpected $ boostedDmg a) + (snd $ f (r-2))
-                                else snd $ f (r-1)
-         both 0 _ = error "Can't boost atk+dmg with no resource"
-         both 1 _ = error "Can't boost atk+dmg with one resource"
-         both 2 f = do h <- boostedAtk d
-                       return $ if h 
-                                then (myExpected $ boostedDmg a) 
-                                else snd $ f 0
-         both r f = do h <- boostedAtk d
-                       return $ if h 
-                                then (myExpected $ boostedDmg a) + (snd $ f (r-3))
-                                else snd $ f (r-2)
+         bdmg _ 0 _ = error "Can't boost damage with no resource"
+         bdmg ah 1 f = do h <- attack d
+                          return $ if h || ah then (myExpected $ boostedDmg a) 
+                                              else snd $ f ah 0
+         bdmg ah r f = do h <- attack d
+                          return $ if h || ah
+                                   then (myExpected $ boostedDmg a) + (snd $ f ah (r-2))
+                                   else snd $ f ah (r-1)
+         both _ 0 _ = error "Can't boost atk+dmg with no resource"
+         both _ 1 _ = error "Can't boost atk+dmg with one resource"
+         both ah 2 f = do h <- boostedAtk d
+                          return $ if h || ah
+                                   then (myExpected $ boostedDmg a) 
+                                   else snd $ f ah 0
+         both ah r f = do h <- boostedAtk d
+                          return $ if h || ah
+                                   then (myExpected $ boostedDmg a) + (snd $ f ah (r-3))
+                                   else snd $ f ah (r-2)
+         hdbt _ 0 _ = error "Can't power attack with no resource"
+         hdbt ah 1 _ = do h <- attack d
+                          return $ if h || ah
+                                   then (myExpected $ damage s)
+                                   else 0
+         hdbt ah r f = do h <- attack d
+                          return $ if h || ah
+                                   then (myExpected $ damage s) + (snd $ f True (r-1))
+                                   else snd $ f ah (r-1)
+         hdba _ 0 _ = error "Can't power attack+boost attack with no resource"
+         hdba _ 1 _ = error "Can't power attack+boost attack with one resource"
+         hdba ah 2 _ = do h <- boostedAtk d
+                          return $ if h || ah
+                                   then (myExpected $ damage s)
+                                   else 0
+         hdba ah r f = do h <- boostedAtk d
+                          return $ if h || ah
+                                   then (myExpected $ damage s) + (snd $ f True (r-2))
+                                   else snd $ f ah (r-2)
+         hdbd _ 0 _ = error "Can't power attack+boost damage with no resource"
+         hdbd _ 1 _ = error "Can't power attack+boost damage with one resource"
+         hdbd ah 2 f = do h <- attack d
+                          return $ if h || ah
+                                   then (myExpected $ boostedDmg s)
+                                   else snd $ f ah 1
+         hdbd ah r f = do h <- attack d
+                          return $ if h || ah
+                                   then (myExpected $ boostedDmg s) + (snd $ f True (r-2))
+                                   else snd $ f ah (r-1)
+         hdda _ 0 _ = error "Can't power attack+boost both with no resource"
+         hdda _ 1 _ = error "Can't power attack+boost both with one resource"
+         hdda _ 2 _ = error "Can't power attack+boost both with two resource"
+         hdda ah 3 f = do h <- boostedAtk d
+                          return $ if h || ah
+                                   then (myExpected $ boostedDmg s)
+                                   else snd $ f ah 1
+         hdda ah r f = do h <- boostedAtk d
+                          return $ if h || ah
+                                   then (myExpected $ boostedDmg s) + (snd $ f True (r-3))
+                                   else snd $ f ah (r-2)
